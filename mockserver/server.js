5f0a2e8f5398ac6bb12659f9a1d1f6be69ac7fbb
Gaurav Bhola <gaurav414u@gmail.com>
2018-02-15 12:47:13 +0530

    whitelisting the local dev server for cors

--- mockserver/server.js
+++ mockserver/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
+const cors = require('cors');
 const app = express();
 const port = process.env.MOCK_SERVER_PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+var whitelist = ['http://localhost:3000'];
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+
+app.get('/api/ping',cors(corsOptions) ,(req, res) => {
+  res.send({ express: 'Pong From Express' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
