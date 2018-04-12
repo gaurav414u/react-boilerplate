2cd2887466dd161020f52d128653fd1c0b63e27c
Gaurav Bhola <gaurav414u@gmail.com>
2018-04-12 13:48:35 +0530

    Adding Main.jsx which wraps the routing

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 import FluxApp from './utils/FluxApp'
 import './styles/index.css'
 
@@ -9,15 +8,13 @@ let fluxApp = new FluxApp() // eslint-disable-line no-unused-vars
 console.log("FluxApp initialized")
 
 // This need to be after FluxApp creation because this will create stores which need alt instance
-let Routes = require('./Routes')
+let Main = require('./components/Main').default
 
 // Disabling service worker for now
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <BrowserRouter>
-        {Routes.routes}
-    </BrowserRouter>
+  <Main />
     ,
     document.getElementById('root')
 )
